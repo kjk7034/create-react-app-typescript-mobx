c07bf09f9d3f9f5d9a4bfd60347b97a35660f2af
kjk7034 <wise7034@gmail.com>
2018-06-21 12:02:41 +0900

    타입추가, response.status 예외처리

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default function request(
-  url: any,
-  method = 'get',
+  url: string,
+  method: string = 'get',
   data?: any,
   config?: any
 ) {
@@ -12,6 +12,29 @@ export default function request(
     data,
     ...config
   })
-    .then(response => response)
-    .catch(error => error)
+    .then(response => {
+      if (response.status !== 200) {
+        throw response
+      }
+      return response
+    })
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log('error.response.data', error.response.data)
+        console.log('error.response.status', error.response.status)
+        console.log('error.response.headers', error.response.headers)
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log('error.request', JSON.stringify(error))
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('etc error', error)
+      }
+      alert(error.message)
+      return Promise.reject(error)
+    })
 }
